2a68d07d588d8c78ef3fce38b2e04bb86ae2cec9
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-08 18:40:03 -0400

    Update Main.java

--- Max_Product/src/Max_Product/Main.java
+++ Max_Product/src/Max_Product/Main.java
@@ -1,11 +1,18 @@
 package Max_Product;
 
+/*
+    This class is Main class to define the user input array. The users can change the hardcoded int array.
+    After compiling the program, it will show the maximum output value which is calculated by the implementation class.
+    Author: Hyejin Kim
+    Date: 9/8/2021
+*/
 public class Main {
     public static void main(String[] args){
+        
         Max_Product MP = new Max_Product();
 
-        int[] userInputs = {-1,4,0,9,3,7};
+        int[] userInputs = {-1,4,0,9,3,7}; //Users can change the input numbers here.
         System.out.println("The maximum product result will be " + MP.calculation(userInputs));
     }
 
-}
+}//Main
