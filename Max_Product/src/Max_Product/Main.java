84afbd7b4752998703a0cee3558afb6cc1e97e70
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-08 18:25:26 -0400

    Update Main.java

--- Max_Product/src/Max_Product/Main.java
+++ Max_Product/src/Max_Product/Main.java
@@ -1,4 +1,11 @@
 package Max_Product;
 
 public class Main {
+    public static void main(String[] args){
+        Max_Product MP = new Max_Product();
+
+        int[] userInputs = {-1,4,0,9,3,7};
+        System.out.println("The maximum product result will be " + MP.calculation(userInputs));
+    }
+
 }
